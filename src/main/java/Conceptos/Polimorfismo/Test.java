622007cf6dbcd538df23254b1505597e42937fc0
elbu <marcobullones@gmail.com>
2024-04-01 11:34:49 +0200

    Version inicial

--- src/main/java/Conceptos/Polimorfismo/Test.java
+++ src/main/java/Conceptos/Polimorfismo/Test.java
@@ -9,28 +9,37 @@ public class Test {
     //          \
     //              C (mC)
     public static void main(String[] args) {
-        /*
+        System.out.println("-- Llamamos a los metodos de las clases por separado");
+        System.out.println("-- A");
         A a = new A();
         a.mA();
 
+        System.out.println("-- B");
         B b = new B();
         b.mA();
+        b.mB();
 
+        System.out.println("-- C");
         C c = new C();
         c.mA();
+        c.mC();
 
+        System.out.println("-- D");
         D d = new D();
         d.mA();
         d.mB();
         d.mD();
-         */
 
+        System.out.println("-- Utilizando el polimorfismo creamos un array de la clase padre (A), donde " +
+                "meteremos instancias de clases que hereden directa o indirectamente de la clase (A)");
         ArrayList<A> listaComun = new ArrayList<>();
         listaComun.add(new A());
         listaComun.add(new B());
         listaComun.add(new C());
         listaComun.add(new D());
 
+        System.out.println("-- Invocamos al metodo mA ya que es el unico que tiene definido el padre!!. " +
+                "Pero en funcion de la instancia sobre la que se encuentre cogera la implementacion de la clase adecuada");
         for(A item : listaComun){
             item.mA();
         }
